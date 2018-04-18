23b29726f94a4999d7b0cb8937fb0443619ada40
coded42 <coded42@gmail.com>
2018-04-18 22:02:49 +0530

    fixed etcd-run

--- all-master/etcd-run.sh
+++ all-master/etcd-run.sh
@@ -11,7 +11,7 @@ export ETCD_VERSION=v3.1.10
 #export ETCD_VERSION=v3.2.18
 curl -sSL https://github.com/coreos/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz | tar -xzv --strip-components=1 -C /usr/local/bin/
 
-rm -rf etcd-$ETCD_VERSION-linux-amd64*
+#rm -rf etcd-$ETCD_VERSION-linux-amd64*
 
 # =============================================
 # ===    generate the environment file that systemd will use
