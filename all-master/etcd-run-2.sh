97e276173a4b1e6b7d7db7036e4a2c1a520662de
coded42 <coded42@gmail.com>
2018-04-18 17:49:19 +0530

    add etcd-run-2

---
+++ all-master/etcd-run-2.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# =============================================
+# ===   install etcd binaries
+# =============================================
+
+mkdir -p /etc/kubernetes/pki/etcd
+cd /etc/kubernetes/pki/etcd
+
+export ETCD_VERSION=v3.1.10
+#export ETCD_VERSION=v3.2.18
+curl -sSL https://github.com/coreos/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz | tar -xzv --strip-components=1 -C /usr/local/bin/
+
+rm -rf etcd-$ETCD_VERSION-linux-amd64*
+
+# =============================================
+# ===    generate the environment file that systemd will use
+# =============================================
+
+# env var
+export PEER_NAME=$(hostname)
+export PRIVATE_IP=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
+
+touch /etc/etcd.env
+echo "PEER_NAME=$PEER_NAME" >> /etc/etcd.env
+echo "PRIVATE_IP=$PRIVATE_IP" >> /etc/etcd.env
+
+
+# =============================================
+# ===   copy the systemd unit file
+# =============================================
+
+cat >/etc/systemd/system/etcd.service <<EOF
+[Unit]
+Description=etcd
+Documentation=https://github.com/coreos/etcd
+Conflicts=etcd.service
+Conflicts=etcd2.service
+
+[Service]
+EnvironmentFile=/etc/etcd.env
+Type=notify
+Restart=always
+RestartSec=5s
+LimitNOFILE=40000
+TimeoutStartSec=0
+
+
+ExecStart=/usr/local/bin/etcd --name ${PEER_NAME} \
+    --data-dir /var/lib/etcd \
+    --listen-client-urls https://${PRIVATE_IP}:2379 \
+    --advertise-client-urls https://${PRIVATE_IP}:2379 \
+    --listen-peer-urls https://${PRIVATE_IP}:2380 \
+    --initial-advertise-peer-urls https://${PRIVATE_IP}:2380 \
+    --cert-file=/etc/kubernetes/pki/etcd/server.pem \
+    --key-file=/etc/kubernetes/pki/etcd/server-key.pem \
+    --client-cert-auth \
+    --trusted-ca-file=/etc/kubernetes/pki/etcd/ca.pem \
+    --peer-cert-file=/etc/kubernetes/pki/etcd/peer.pem \
+    --peer-key-file=/etc/kubernetes/pki/etcd/peer-key.pem \
+    --peer-client-cert-auth \
+    --peer-trusted-ca-file=/etc/kubernetes/pki/etcd/ca.pem \
+    --initial-cluster master0=https://10.0.0.4:2380,master1=https://10.0.0.5:2380,master2=https://10.0.0.6:2380 \
+    --initial-cluster-token my-etcd-token \
+    --initial-cluster-state new
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# launch etcd
+systemctl daemon-reload
+systemctl start etcd
+
+# check if launched successfully
+systemctl status etcd
+
